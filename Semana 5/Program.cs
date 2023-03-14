4e3130b66f3c593dec0c5815c13f129fd7c7e5f6
JepCast <ycastillop2@miumg.edu.gt>
2023-03-14 09:28:47 -0600

    Agregue el calculo para obtener el factorial de n numeros junto con unos comentarios.

--- Semana 5/Program.cs
+++ Semana 5/Program.cs
@@ -1,27 +1,29 @@
 ﻿
 string name;
-int num4;
+int num4,facn;
 double radio,area, perimetro,an, al;
 
+//Primero que nada se le solicita al usuario su nombre para así darle una pequeña bienvenida.
 Console.WriteLine("Primero que nada, dime, ¿Como te llamas?");
     name = Console.ReadLine();
 Console.WriteLine($"Bienvenid@ {name}, a continuación se te solicitarán ciertos por faor insertalos de manera correcta.");
 
-
+//Calculo del radio de un circulo.
+Console.WriteLine("\nPor favor inserta a continuación el radio del circulo que deseas calcular:");
+    radio = Convert.ToDouble(Console.ReadLine());
 static double AreaC(double radio)
 {
-    //Es un static double por que trabajare con datos double
-    double area=Math.PI*Math.Pow(radio,2);
+    //Es un static double por que trabajare con datos double.
+    double area = Math.PI * Math.Pow(radio, 2);
     return area;
 }
 //En este punto el Math.PI es para calcular el valor de π seguido de un Math.Pow el cual me devuelve un valor especifico
 //Claramente elevado a una potencia en este caso sería radio^2
 
-Console.WriteLine("\nPor favor inserta a continuación el radio del circulo que deseas calcular:");
-    radio = Convert.ToDouble(Console.ReadLine());
-    area = AreaC(radio);
+area = AreaC(radio);
 Console.WriteLine($"El area del circulo es en total: \n{area}");
 
+//Obtención del perimetro de un triangulo.
 Console.WriteLine("Por favor inserta a continuación la altura y el ancho del perimetro que deseas calcular:");
 Console.ReadLine();
 
@@ -44,7 +46,7 @@ Console.WriteLine("Por favor inserta a continuación la lista de numeros enteros
 
 
 
-
+//Determinación si un numero es par o impar.
 Console.WriteLine("Por favor inserta a continuación el numero que quieres saber si es par o impar:");
 num4=Convert.ToInt32(Console.ReadLine());
 
@@ -60,5 +62,18 @@ else
 
 Console.WriteLine("Por favor inserta a continuación los numeros que deseas sumar:");
 
-
+//Calculo del factorial de n numero.
 Console.WriteLine("Por favor inserta a continuación el numero del cual quieres obtener el factorial:");
+facn=Convert.ToInt32(Console.ReadLine());
+
+static int Fact(int facn)
+{ 
+    int Fact = 1;
+        for (int i = 0; i < facn; i++)
+        {
+            Fact *= i;
+        }
+        return Fact;
+}
+Console.WriteLine($"El factorial de {facn} es: {Fact}");
+Console.ReadKey();
